660a8a322e30e151367ba863316b378d81ec3d90
Folawewo Anthony <famwallexie@gmail.com>
2023-08-05 04:40:59 +0100

    starting server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./app");
+
+const PORT = process.env.PORT || 80;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Starting server on port ${PORT}!`);
+});
