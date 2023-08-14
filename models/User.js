e96ce77ed03545e2dfe02f04acd32f073c93586e
Folawewo Anthony <famwallexie@gmail.com>
2023-08-14 03:10:21 +0100

    user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userschema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userschema);
+
+module.exports = User;
