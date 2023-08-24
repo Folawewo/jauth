206831c83b2f4108fa794b0f5c688bc031059bf1
Folawewo <famwallexie@gmail.com>
2023-08-24 22:49:09 +0100

    register user

--- routes/user.js
+++ routes/user.js
@@ -3,3 +3,22 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const router = express.Router();
 
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User successfully registered!" });
+  } catch (error) {
+    res.status(500).json({ error: "An error occured" });
+  }
+});
+
+module.exports = router;
